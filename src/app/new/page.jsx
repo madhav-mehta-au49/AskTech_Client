10a85c604e03f00ae606467b4b44544f435945da
madhav-mehta-au49 <mehtamadhav2002@gmail.com>
2024-03-14 14:15:16 +0530

    first commit

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -4,57 +4,126 @@ import styles from './new.module.css'
 
 const page = () => {
   return (
-    <>
-    <div className={`${styles.blogCard} ${styles.alt}`}>
-      <div className={styles.meta}>
-        <div className={styles.photo} style={{ backgroundImage: `url(https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406__480.jpg)` }}></div>
-        <ul className={styles.details}>
-          <li className={styles.author}><a href="#">user name</a></li>
-          <li className={styles.date}>Nov. 13, 2020</li>
-          <li className={styles.tags}>
-            <ul>
-              <li><a href="#">C++</a></li>
-              <li><a href="#">Python</a></li>
-              <li><a href="#">Java</a></li>
-              <li><a href="#">JavaScript</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.description}>
-        <h1>Learning to Code in python</h1>
-        <h2>Lorem ipsum dolor sit amet</h2>
-        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
-        <p className={styles.readMore}>
-          <a href="#">Read More</a>
-        </p>
-      </div>
-    </div>
-    <div className={styles.blogCard}>
-      <div className={styles.meta}>
-        <div className={styles.photo} style={{ backgroundImage: `url(https://cdn.pixabay.com/photo/2015/07/02/09/52/interior-design-828545_960_720.jpg)` }}></div>
-        <ul className={styles.details}>
-          <li className={styles.author}><a href="#">user name</a></li>
-          <li className={styles.date}>Nov. 13, 2020</li>
-          <li className={styles.tags}>
-            <ul>
-              <li><a href="#">Buy</a></li>
-              <li><a href="#">channels</a></li>
-              <li><a href="#">ads</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.description}>
-        <h1>Apple TV Plus cost, shows, channels, devices</h1>
-        <h2>Lorem ipsum dolor sit amet</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
-        <p className={styles.readMore}>
-          <a href="#">Read More</a>
-        </p>
-      </div>
-    </div>
-  </>
+  //   <div>
+  //  <section className={styles['gradient-pp']}>
+  //     <div className="container">
+  //       <h1 className={styles.title + ' title text-center pt-5'}>ADTECH AND MARKETING TECHNOLOGY SOLUTIONS</h1>
+  //     </div>
+  //     <div className={styles['position-absolute']}>
+  //       <svg className={styles.waves} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+  //         viewBox="0 24 150 40" preserveAspectRatio="none" shapeRendering="auto">
+  //         <defs>
+  //           <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+  //         </defs>
+  //         <g className={styles['moving-waves']}>
+  //           <use xlinkHref="#gentle-wave" x="48" y="-1" fill="rgba(255,255,255,0.40)" />
+  //           <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.35)" />
+  //           <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.25)" />
+  //           <use xlinkHref="#gentle-wave" x="48" y="8" fill="rgba(255,255,255,0.20)" />
+  //           <use xlinkHref="#gentle-wave" x="48" y="13" fill="rgba(255,255,255,0.15)" />
+  //           <use xlinkHref="#gentle-wave" x="48" y="16" fill="rgba(255,255,255,1)" />
+  //         </g>
+  //       </svg>
+  //     </div>
+  //   </section>
+    
+  //   <div className={styles.blogCard}>
+  //     <div className={styles.meta}>
+  //       <div className={styles.photo2} ></div>
+  //       <ul className={styles.details}>
+  //         <li className={styles.author}><a href="#"></a></li>
+  //         <li className={styles.date}></li>
+  //         <li className={styles.tags}>
+  //           <ul>
+  //             <li><a href="#"></a></li>
+  //             <li><a href="#"></a></li>
+  //             <li><a href="#"></a></li>
+  //           </ul>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //     <div className={styles.description}>
+        
+  //       <p>AskTech is a full-service reliable AdTech and Marketing Technology software development partner with extensive experience helping clients build platforms for a variety of purposes, including purchasing and selling ads, accessing ad inventory, gaining enhanced consumer insights, and assessing the effectiveness of multiple-channel media campaigns. From disruptive startups to leading advertising and media companies, we have developed, tested and maintained world-class Online Advertising and Marketing Solutions.</p>
+        
+  //     </div>
+    
+  //     </div>
+  //     <div className={styles.blogCard}>
+  //     <div className={styles.meta}>
+  //       <div className={styles.photo} ></div>
+  //       <ul className={styles.details}>
+  //         <li className={styles.author}><a href="#"></a></li>
+  //         <li className={styles.date}></li>
+  //         <li className={styles.tags}>
+  //           <ul>
+  //             <li><a href="#"></a></li>
+  //             <li><a href="#"></a></li>
+  //             <li><a href="#"></a></li>
+  //           </ul>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //     <div className={styles.description}>
+  //       <h1>Adtech and Marketing Technology State of The Art</h1>
+       
+  //       <p>AdTech and Marketing Technology are becoming integral parts of successful marketing strategies. Marketers and advertisers rely heavily on personalization to drive performance and, ultimately, sales. In an era where consumers are constantly switching between marketing channels and channels of interaction, it may be difficult to cut through the noise and provide relevant personalized experiences at every touch point of the consumer journey.</p>
+        
+  //     </div>
+    
+  //     </div>
+  //     <div className={styles.container}>
+  //      <h1 className={styles.heading} >Custom AdTech Solutions Expertise</h1> 
+  //     <hr className={styles.hr} />
+  //     <div className={styles.row}>
+  //       <div className={styles.col}>
+  //         <div className={styles.blogCardone}>
+  //           <div className={styles.blogCardImageone}>
+  //             <img src="https://abhishekdana1999.github.io/Mywebsite/img/blog_1.png" alt="" />
+  //           </div>
+  //           <div className={styles.blogCardContentone}>
+             
+  //             <ul>
+  //               <li>Expertise in custom AdTech solutions</li>
+  //               <li>Developing Supply-side Platforms (SSPs)</li>
+  //               <li>Development of Demand-Side Platforms (DSP)</li>
+  //               <li>Platforms for managing data (DMPs)</li>
+  //               <li>Solutions for targeting and retargeting</li>
+  //               <li>Solutions for predictive analytics</li>
+  //               <li>Ad-serving on a dynamic basis</li>
+  //               <li>Platforms for programmatic advertising and real-time bidding</li>
+  //               <li>Providing 360-degree solutions for the consumer journey</li>
+  //             </ul>
+              
+  //           </div>
+  //         </div>
+  //       </div>
+  //       {/* Add more blog cards here */}
+  //       <div className={`${styles.col} ${styles.colLg4} ${styles.colMd6} ${styles.colSm12}`}>
+  //         <div className={styles.blogCardone}>
+  //           <div className={styles.blogCardImageone}>
+  //             <img src="" alt="" />
+  //           </div>
+  //           <div className={styles.blogCardContentone}>
+              
+  //              <ul>
+  //               <li>Expertise in custom marketing solutions</li>
+  //               <li>Platforms for marketing analytics</li>
+  //               <li>Solutions for predictive analytics</li>
+  //               <li>Software for marketing automation</li>
+  //               <li>Platforms for managing social media</li>
+  //               <li>Referral and loyalty platforms</li>
+  //               <li>An engine for personalization</li>
+  //               <li>Recommendation engines tailored to your needs</li>
+  //               <li>Reporting and data visualization for campaigns</li>
+  //              </ul>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  //     </div>
+  <p>hello</p>  
   )
 }
 
